f8ca7c497831ae8706a39d81a604c2f9ff46f7fa
reshiamir <amir.reshi@sapient.com>
2019-05-30 11:10:57 +0530

    complete crud ngxs app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FormComponent } from "./form/form.component";
+import { ListComponent } from "./list/list.component";
+import { NgxsModule } from "@ngxs/store";
+import { TodoState } from "./state/todo.state";
+
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormComponent, ListComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([TodoState]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
